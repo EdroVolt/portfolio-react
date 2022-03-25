08f4c1e9a131bd93f9247f7f69f93087bffc1427
Ahmed Edrees <ahmededrees30@gmail.com>
2022-03-25 21:43:21 +0200

    fix: compress images

--- src/components/Portfolio/PortfolioCard/PortfolioCard.jsx
+++ src/components/Portfolio/PortfolioCard/PortfolioCard.jsx
@@ -3,7 +3,12 @@ import "./protfolioCard.css";
 export default function PortfolioCard(props) {
   return (
     <div className="card portfolio-card" style={{ width: "18rem" }}>
-      <img src={props.img} className="card-img-top" alt="project" />
+      <img
+        loading="lazy"
+        src={props.img}
+        className="card-img-top"
+        alt="project"
+      />
     </div>
   );
 }
