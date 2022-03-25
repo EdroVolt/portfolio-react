b4be980086cca93971e4966071897ea466f3f315
Ahmed Edrees <ahmededrees30@gmail.com>
2022-03-25 21:09:12 +0200

    feat: add floating download resume btn

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "./app.css";
 import Header from "./Header/Header";
+import FloatingButton from "./FloatingButton/FloatingButton";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <FloatingButton />
     </div>
   );
 }
---
+++ src/components/FloatingButton/FloatingButton.jsx
@@ -0,0 +1,14 @@
+import "./floatingButton.css";
+
+export default function FloatingButton() {
+  return (
+    <a
+      href="https://doc-0k-5k-docs.googleusercontent.com/docs/securesc/f8e785cpbh01406dtt71fe91otiqjoig/637dt2urgrsh141ke05p9mmk407356ik/1648234725000/12342372647644256506/12342372647644256506/1ZshgdhPlSz7lfQlhnE2N2XAkm-3oldrP?e=download&ax=ACxEAsbrFHNOmSolBo9BPsEiJ9N_cwbj1gCRV4Ak-SJPSW1slZtCqV5eGcwqAJ-OTmi6gMx74ZTcmurAFnbGMQjLy401TIXbtRJUu3esFglSSnaWWgQ6M8m3nJ1jnAgrQmDtslTJGUlC6BLBd9kXQI2BtQRlyGMMDjRYJAgAjg0wr0m5QRlc5sCUOxbsyGkaXHo_QdFsbPXI_SwQsPvbzlVWkl174ibN38iIKaqRXbgJCZIak7TT5dftIdKFRIAzPQ-bgqZ2fgeGZyV6ySqWDFYQi7iAQhpuiCKwqYeCkXTJArt0Mm1LMT0yMfoJtzUm2p-dXZAi4cvgIXkFVOTrn6kMlse-FV4-r4PSrcyz5ig_yO4uCEw_d4fhYR6MsC7lcfIMj7RwJJxHK0M8odO96oNOS1xlps3qydxL6Vw1Pgqyy4ho67iPrUsUqKh6djFy0YgtZE38_ZafSpNW-eN8zeuE9AS0-Yu4sjbIa0q0O24g6PfikUyJx6nJYlGpyLMWbRNnzyYFlCy6vbAV9iuE9b6D5drZVs3MQhW4C0PSzhD0bQ8zRZOGFrLerwUbmXD6kHrQrSNNCKvWhBP-neWeVAlYQnisQEnJFL6ANAqBLL0geGBpvNc5bRL909z2U0Do8Gx97bhgu5iMbdNp1cQbCU1wq5I9wEIg-QUu8MVFCqav75M_fMXnIrmO3z6cVOpcYXT9-4-6McowfP55LQ&authuser=0&nonce=76bhoa5v3v3sg&user=12342372647644256506&hash=dfiud6man8b8p9dpc5u1hl7b4drp6fst"
+      className="btn-floating btn btn-success rounded"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Resume <i className="fa-arrow-trend-down"></i>
+    </a>
+  );
+}
