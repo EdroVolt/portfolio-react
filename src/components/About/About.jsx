f914ab735324c5d2af9a4055b71ff601f55bf9fe
Ahmed Edrees <ahmededrees30@gmail.com>
2022-03-25 21:10:03 +0200

    feat: create about section

---
+++ src/components/About/About.jsx
@@ -0,0 +1,27 @@
+import "./about.css";
+
+export default function About() {
+  return (
+    <div className="section container">
+      <div className="row">
+        <h2 className="col-4 main-question">Who am I ?</h2>
+
+        <div className="col-8">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium
+            commodi consequuntur quae. Earum dolorem est illum ratione dolorum
+            nulla error, labore repellat ea? Eaque laborum ea eligendi
+            accusamus, officia fugit? Lorem, ipsum dolor sit amet consectetur
+            adipisicing elit. Fuga libero sapiente corporis molestias velit
+            temporibus ab optio commodi amet ea inventore et voluptates quo
+            dicta, possimus voluptas similique labore magnam! Lorem ipsum dolor,
+            sit amet consectetur adipisicing elit. Impedit quaerat dolorum
+            laudantium inventore omnis. Distinctio atque veniam libero id,
+            veritatis laudantium nostrum in nisi vero voluptatibus officiis,
+            velit omnis ratione.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,14 @@ import "@fortawesome/fontawesome-free/css/all.css";
 import "./app.css";
 import Header from "./Header/Header";
 import FloatingButton from "./FloatingButton/FloatingButton";
+import About from "./About/About";
 
 function App() {
   return (
     <div className="app">
       <Header />
       <FloatingButton />
+      <About />
     </div>
   );
 }
