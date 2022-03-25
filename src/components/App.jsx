5f06533349c16b5f39357160db7673fa3710d804
Ahmed Edrees <ahmededrees30@gmail.com>
2022-03-25 21:06:44 +0200

    feat: create app component

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.css";
+import "./app.css";
+
+function App() {
+  return <div className="app"></div>;
+}
+
+export default App;
